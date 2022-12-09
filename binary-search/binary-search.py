d49ea12a1677522441be03c6674805d783ee62ca
Rawad Shakeb <rawadabusaleh0@gmail.com>
2022-12-09 12:20:27 +0200

    Added binary_search_recursive

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -13,13 +13,28 @@ def binary_search(arr, x):
             return mid
     return -1
 
+def binary_search_recursive(arr, s, e, x):
+    # My code here
+    if s == e:
+        return arr[s]
+
+    m = (e+s)//2 
+    if arr[m] < x:
+        return binary_search_recursive(arr, m+1, e, x)
+    elif arr[m] > x:
+        return binary_search_recursive(arr, s, m-1, x)
 
 if __name__ == "__main__":
     arr = [ 2, 3, 4, 10, 40 ]
-    x = 11
+    s = 0
+    e = len(arr) - 1
+
+    x = 40
 
     my_index = binary_search(arr,x)
     if my_index == -1:
         print("Element is not existed")
     else:
         print("Element at index", str(my_index))
+
+    print(binary_search_recursive(arr,s,e,x))
